80bc958059b87c9a1318ffe968e4fcebc5d03e2b
RizalNururrahman <RizalNururrahman@gmail.com>
2024-04-24 13:01:25 +0700

    Tugas3

--- app/src/main/java/com/example/tugas2coba1/Login.java
+++ app/src/main/java/com/example/tugas2coba1/Login.java
@@ -1,12 +1,16 @@
 package com.example.tugas2coba1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.content.Intent;
 
 public class Login extends AppCompatActivity {
 
@@ -20,5 +24,16 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void pindah2(View view) {
+        Intent pindah2 = new Intent(Login.this, layer3.class);
+        startActivity(pindah2);
+    }
+
+
+    public void pindah3(View view) {
+        Intent pindah3 = new Intent(Login.this, layer4.class);
+        startActivity(pindah3);
     }
 }
--- app/src/main/java/com/example/tugas2coba1/layer3.java
+++ app/src/main/java/com/example/tugas2coba1/layer3.java
@@ -1,22 +1,22 @@
 package com.example.tugas2coba1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.view.View;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class layer3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_layer3);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void pindah(View view) {
-        Intent pindah = new Intent(MainActivity.this, Login.class);
-        startActivity(pindah);
+    public void pindah3(View view) {
+        Intent pindah3 = new Intent(layer3.this, Login.class);
+        startActivity(pindah3);
     }
 }
